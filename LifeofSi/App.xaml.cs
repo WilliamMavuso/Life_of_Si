47f158448c6c926db4bdf939f0209fdea984702a
William <admin@Admins-MacBook-Air.local>
2020-03-14 15:50:21 +0200

    Initial commit

---
+++ LifeofSi/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LifeofSi
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartingPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LifeofSi/StartingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LifeofSi
+{
+    public partial class StartingPage : ContentPage
+    {
+        public StartingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
