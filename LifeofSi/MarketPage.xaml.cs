490ecd70ce649c0b13fa9433d163e6a9a64ae635
admin <180256@virtualwindow.co.za>
2020-03-14 23:07:53 +0200

    Feed page front_end

---
+++ LifeofSi/MarketPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LifeofSi
+{
+    public partial class MarketPage : ContentPage
+    {
+        public MarketPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
