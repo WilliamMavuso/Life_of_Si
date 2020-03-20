42a6d01c180c74a5c62a722fef5fa43430ce0b1f
admin <180256@virtualwindow.co.za>
2020-03-20 21:51:27 +0200

    Flies page front_end

---
+++ LifeofSi/Objects/Transform.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//namespace LifeofSi.Objects
+//{
+//    public class Transform
+//    {
+
+//        const string transformStateKey = "transformState";
+
+//        public TransformState CurrentTransformState
+//        {
+//            get
+//            {
+//                if (App.Current.Properties.ContainsKey(transformStateKey))
+//                {
+//                    return TransformState.GetTransformState((string)App.Current.Properties[transformStateKey];
+//                }
+//                else
+//                {
+//                    return TransformState.stage1;
+//                }
+//            }
+
+//            set
+//            {
+//                App.Current.Properties[transformStateKey] = TransformState.GetTransformString(value);
+
+//            }
+
+//        }
+
+//        public Transform()
+//        {
+//        }
+//    }
+//}
