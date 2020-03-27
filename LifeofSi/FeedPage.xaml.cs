3fbdf413a9ab483df93a7f4e4b63b4f0b86c1718
admin <180256@virtualwindow.co.za>
2020-03-27 13:31:18 +0200

    feedpage functionality

--- LifeofSi/FeedPage.xaml.cs
+++ LifeofSi/FeedPage.xaml.cs
@@ -7,15 +7,19 @@ namespace LifeofSi
 {
     public partial class FeedPage : ContentPage
     {
+
+        int clickTotal;
+
         public FeedPage()
         {
             InitializeComponent();
         }
 
-        //public static class userDetails
-        //{
-        //    public static string Name = "Will";
-        //}
+        void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            clickTotal += 1;
+            leaves.Text = $"{clickTotal} /100";
+        }
 
         async void LeafButton_Clicked(System.Object sender, System.EventArgs e)
         {
